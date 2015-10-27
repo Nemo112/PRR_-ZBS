1488b6af963275653ecf1c31cadac38e9391ac1d
nemo <nemo@nemor.cz>
2015-10-27 11:09:27 +0100

    Oprava testování sekvenční částí

--- generate_results.sh
+++ generate_results.sh
@@ -3,13 +3,13 @@ ptht=`pwd`;
 arrl=`ls ZBS*/main`;
 tsar=`ls testovaci_data/*.txt`;
 #for i in ${arrl[@]};do
-	i="./ZBS_var/main";
-	a=8;
+	# i="./ZBS_var/main";
+	a=3;
 	while [[ $a -lt 20 ]];do
 		for j in ${tsar[@]};do
 			k=0;
-			while [[ k -lt 3 ]];do
-				eval "./$i $ptht\"/\"$j $a" > outputs/`echo $j | cut -d'/' -f 2 | cut -d. -f 1`_$a.txt &
+			while [[ k -lt 4 ]];do
+				(time ./ZBS_final/main $ptht/$j $a) |& tee outputs/`echo $j | cut -d'/' -f 2 | cut -d. -f 1`_$a.txt 1>/dev/null &
 				k=$(($k+1));
 			done;
 			wait;
